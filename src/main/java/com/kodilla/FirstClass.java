bedf2950f2db07baec7df3d330494f98f6721bb4
kaszakinga90 <kaszakinga90@gmail.com>
2019-07-05 12:51:02 +0200

    po zmianach

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,48 @@
 package com.kodilla;
 
+import java.util.Objects;
+
 public class FirstClass {
+
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FirstClass)) return false;
+        FirstClass that = (FirstClass) o;
+        return Objects.equals(author, that.author) &&
+                Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, title);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "FirstClass{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    System.out.println("tekst");
 }
+
+
--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -3,6 +3,12 @@ package com.kodilla;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 @SpringBootApplication
 public class KodillaCourseApplication {
 
@@ -10,4 +16,6 @@ public class KodillaCourseApplication {
         SpringApplication.run(KodillaCourseApplication.class, args);
     }
 
-}
+    LinkedList<Book> theList = new LinkedList<Book>();
+
+    System.out.println(theList.size());
