c6b2f9875a3420b0dc05d9ca9c6fdc52d09bee0b
kaszakinga90 <kaszakinga90@gmail.com>
2019-07-09 17:49:55 +0200

    Task 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int wynik1 = calculator.addAToB(3,8);
+        System.out.println(wynik1);
+
+        int wynik2 = calculator.substractAFromB(6,2);
+        System.out.println(wynik2);
+
+        System.out.println("Test - pierwszy test jednostkowy:");
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    {
+        int a;
+        int b;
+
+        public int addAToB(int a, int b){
+            return a + b;
+        }
+
+        public int substractAFromB(int a, int b){
+            return a - b;
+        }
+    }
+}
+
