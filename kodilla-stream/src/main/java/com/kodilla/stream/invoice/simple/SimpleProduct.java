b181c7fa2844929ba2d505e8ae7c42b803d9e5b9
kaszakinga90 <kaszakinga90@gmail.com>
2019-08-13 12:57:24 +0200

    modul 7.6

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/invoice/simple/SimpleProduct.java
@@ -0,0 +1,34 @@
+package com.kodilla.stream.invoice.simple;
+
+public final class SimpleProduct {
+    private final String productName;
+    private final double productPrice;
+
+    public SimpleProduct(final String productName, final double productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SimpleProduct)) return false;
+
+        SimpleProduct that = (SimpleProduct) o;
+
+        return getProductName() != null ? getProductName().equals(that.getProductName()) : that.getProductName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getProductName() != null ? getProductName().hashCode() : 0;
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/portfolio/User.java
@@ -0,0 +1,41 @@
+package com.kodilla.stream.portfolio;
+
+public final class User {
+    private final String username;
+    private final String realname;
+
+    public User(final String username, final String realname) {
+        this.username = username;
+        this.realname = realname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return username != null ? username.equals(user.username) : user.username == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return username != null ? username.hashCode() : 0;
+    }
+}
