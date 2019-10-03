7ae6163aeb9bc77cdea4cb6aa44d38d49c0d2e38
kaszakinga90 <kaszakinga90@gmail.com>
2019-10-03 19:17:54 +0200

    do poprawki

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -3,9 +3,9 @@ package com.kodilla.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringRunner {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringRunner.class, args);
     }
