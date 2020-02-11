18e267c97b93db3af57116ae80850824116527e3
kaszakinga90 <kaszakinga90@gmail.com>
2020-02-11 12:08:57 +0100

    Task 31.2

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing2.google;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleTestingApp {
+    public static final String SEARCHFIELD = "q";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.google.com");
+
+        WebElement searchfield = driver.findElement(By.name(SEARCHFIELD));
+        searchfield.sendKeys("Kodilla");
+        searchfield.submit();
+    }
+}
